061bb47bfd47267009c44513287b062187dcab3a
Arne Nägele <a.naegele@AD.SINGEN-HGS.LOGODIDACT.NET>
2021-11-11 15:54:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/github_projekt/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.github_projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
